a788a3db66de7777f1ef9f648b69669a77b47791
Sxbvstivn <sbstnkrk@gmail.com>
2023-09-27 19:55:17 +0200

    NET2.1_3

---
+++ KategoriaWidok.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+
+namespace NET_Zadanie_2
+{
+    public partial class KategoriaWidok : Window
+    {
+        public KategoriaWidok(XmlElement selectedCategory)
+        {
+            InitializeComponent();
+
+            string categoryName = selectedCategory.GetAttribute("Nazwa");
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filename: "C:\\Users\\YVKVZV\\source\\repos\\NET Zadanie 3\\NET Zadanie 3\\Bronie.xml");
+            XmlNodeList weapons = xmlDoc.SelectNodes($"//Kategoria[@Nazwa='{categoryName}']/Podkategoria/Bron");
+
+            // Przypisz dane do kontrolki, która ma wyświetlać broń (na przykład DataGrid)
+            MyDataGrid.ItemsSource = weapons;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+
+namespace NET_Zadanie_2
+{
+    public partial class MainWindow : Window
+    {
+        ListBox lista;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lista = (ListBox)FindName("Kategorie");
+        }
+
+        private void WidokWybranejKategorii(object sender, RoutedEventArgs e)
+        {
+            // Pobieramy wybraną kategorię jako XmlElement
+            XmlElement wybrana = (XmlElement)lista.SelectedItem;
+
+            // Tworzymy nową instancję KategoriaWidok i przekazujemy wybraną kategorię
+            KategoriaWidok kategoriaWidok = new KategoriaWidok(wybrana);
+
+            // Wyświetlamy nowe okno
+            kategoriaWidok.Show();
+        }
+    }
+}
